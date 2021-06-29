bb56392ec2727ecc8455c033a863c186e4f04782
L.B <mrlawrencelane@gmail.com>
2021-06-29 16:30:32 -0400

    form

---
+++ themes/emdash/static/js/contact.js
@@ -0,0 +1,9 @@
+  function openContact() {
+    var element = document.getElementById("contact-form");
+    element.classList.toggle("is-active");
+  };
+
+  function closeContact() {
+    var element = document.getElementById("contact-form");
+    element.classList.toggle("is-active");
+  };
